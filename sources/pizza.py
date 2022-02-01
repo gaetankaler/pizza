f4c681f52187ed66ade0a8a8feea34e145d3672f
gaetankaler <gaga84120@gmail.com>
2022-02-01 09:57:43 +0300

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'my_pizza': {
+        'ingredients': {'olive': 3, 'ham': 4, 'onion': 3},
+        'price': 12,
+}
 }
